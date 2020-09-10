cbf3d9cc229de3773a1f430cb29f5a31b0804815
Harjotsinghh <harjotsingh206@gmail.com>
2020-09-10 21:18:01 +0530

    Updates

---
+++ precache-manifest.3485169685f84072a6518210a1522309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602ef515414a10466adde5d771c47ca3",
+    "url": "/tittaktoe/index.html"
+  },
+  {
+    "revision": "885895e3ccd650893c87",
+    "url": "/tittaktoe/static/css/main.2dd35dc8.chunk.css"
+  },
+  {
+    "revision": "e1044aa12e7c87171751",
+    "url": "/tittaktoe/static/js/2.142e2909.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tittaktoe/static/js/2.142e2909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885895e3ccd650893c87",
+    "url": "/tittaktoe/static/js/main.1ef020e7.chunk.js"
+  },
+  {
+    "revision": "0ff0b8647a98ca39c9cd",
+    "url": "/tittaktoe/static/js/runtime-main.0afe2287.js"
+  }
+]);
